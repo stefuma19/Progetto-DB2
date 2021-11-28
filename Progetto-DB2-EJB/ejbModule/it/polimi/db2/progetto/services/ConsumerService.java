8949d242ec10bea7e339aad3aef37a53f24b3f96
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-11-28 17:53:22 +0100

    implemented index.html with skip and registration button (without named query to registrate, wip)

--- Progetto-DB2-EJB/ejbModule/it/polimi/db2/progetto/services/ConsumerService.java
+++ Progetto-DB2-EJB/ejbModule/it/polimi/db2/progetto/services/ConsumerService.java
@@ -33,8 +33,8 @@ public class ConsumerService {
 	}
 
 	
-	public String registrate(String email, String username, String password) {
+	public boolean registrate(String email, String username, String password) throws CredentialsException{
 		//TODO: da fare
-		return null;
+		return true;
 	}
 }
--- Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/Registrate.java
+++ Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/Registrate.java
@@ -1,5 +1,90 @@
 package it.polimi.db2.progetto.controllers;
 
-public class Registrate {
+import java.io.IOException;
 
+import javax.ejb.EJB;
+import javax.persistence.NonUniqueResultException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.db2.progetto.exceptions.CredentialsException;
+import it.polimi.db2.progetto.services.ConsumerService;
+
+@WebServlet("/Registrate")
+public class Registrate extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	@EJB(name = "it.polimi.db2.progetto.services/ConsumerService")
+	private ConsumerService consumerService;
+	
+	public Registrate() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = null;
+		String password = null;
+		String email = null;
+		try {
+			username = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			password = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
+			email = StringEscapeUtils.escapeJava(request.getParameter("email"));
+			
+			if (username == null || password == null || email == null || username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+				throw new Exception("Missing or empty credential value");
+			}
+
+		} catch (Exception e) {
+			// for debugging only e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing registration credential value");
+			return;
+		}
+		boolean isRegistrated = false;
+		try {
+			// query db to authenticate for user
+			isRegistrated = consumerService.registrate(email, username, password);
+		} catch (CredentialsException e) {
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not registrate");
+			return;
+		}
+
+		// If the user exists, add info to the session and go to home page, otherwise
+		// show login page with error message
+
+		String path;
+
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		if (!isRegistrated) {
+			ctx.setVariable("errorMsgReg", "Impossible to registrate with these credentials");
+		} else {
+			ctx.setVariable("errorMsgReg", "Registration completed, please login");
+		}
+		path = "/index.html";
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	public void destroy() {
+	}
 }

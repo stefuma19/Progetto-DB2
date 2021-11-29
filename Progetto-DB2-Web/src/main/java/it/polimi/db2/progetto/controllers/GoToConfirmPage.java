1e1683a4debb8a72376149de8e145a4c1cd9456d
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-11-29 15:37:40 +0100

    Added button to confirm invalid orders from home

--- Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/GoToConfirmPage.java
+++ Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/GoToConfirmPage.java
@@ -1,5 +1,57 @@
 package it.polimi.db2.progetto.controllers;
 
-public class GoToConfirmPage {
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.db2.progetto.entities.Order;
+import it.polimi.db2.progetto.entities.ServicePackage;
+
+@WebServlet("/GoToConfirmPage")
+public class GoToConfirmPage extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	
+	public GoToConfirmPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String path = "/WEB-INF/confirm.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());		
+		//ctx.setVariable("servicePackages", servicePackages);
+		//ctx.setVariable("invalidOrders", invalidOrders);
+		templateEngine.process(path, ctx, response.getWriter());
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }

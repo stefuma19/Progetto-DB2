e57cf39762b5368add9ab449ba802d08824c51cc
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-11-29 15:15:44 +0100

    Implemented skipLogin, delete session variables

---
+++ Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/SkipLogin.java
@@ -0,0 +1,47 @@
+package it.polimi.db2.progetto.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+@WebServlet("/SkipLogin")
+public class SkipLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	
+	public SkipLogin() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.getSession().setAttribute("consUsername", "");
+		request.getSession().setAttribute("consIsInsolvent", false);
+		String path = getServletContext().getContextPath() + "/GoToHomePage";
+		response.sendRedirect(path);
+	}
+
+	public void destroy() {
+	}
+
+}

5891b12f63b61b21684bd9cc4a995777e3bb8b73
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-12-01 16:39:29 +0100

    Implemented function order :)

--- Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/ConfirmOrder.java
+++ Progetto-DB2-Web/src/main/java/it/polimi/db2/progetto/controllers/ConfirmOrder.java
@@ -2,8 +2,92 @@ package it.polimi.db2.progetto.controllers;
 
 
 
-//dopo aver cliccato il pulsante per creare (pagare) ordine, invoca l'orderservice.createOrder
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class ConfirmOrder {
+import javax.ejb.EJB;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.db2.progetto.entities.FixedPhone;
+import it.polimi.db2.progetto.entities.MobileInternet;
+import it.polimi.db2.progetto.entities.OptionalProduct;
+import it.polimi.db2.progetto.entities.Order;
+import it.polimi.db2.progetto.entities.ServicePackage;
+import it.polimi.db2.progetto.entities.ValidityPeriod;
+import it.polimi.db2.progetto.services.ConsumerService;
+import it.polimi.db2.progetto.services.OptionalProductService;
+import it.polimi.db2.progetto.services.OrderService;
+import it.polimi.db2.progetto.services.ServicePackageService;
+import it.polimi.db2.progetto.services.ValidityPeriodService;
+
+@WebServlet("/ConfirmOrder")
+public class ConfirmOrder extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	
+	@EJB(name = "it.polimi.db2.progetto.services/OrderService")
+	private OrderService orderService;
+	@EJB(name = "it.polimi.db2.progetto.services/ConsumerService")
+	private ConsumerService consumerService;
+	//dopo aver cliccato il pulsante per creare (pagare) ordine, invoca l'orderservice.createOrder
+	
+	public ConfirmOrder() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//String temp = (String) request.getSession().getAttribute("sd");
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");  
+		
+		try {
+			orderService.createOrder(consumerService.findConsumerById((String)request.getSession().getAttribute("consUsername")), 
+					(ServicePackage)request.getSession().getAttribute("sp"), 
+					(ValidityPeriod)request.getSession().getAttribute("vp"), 
+					true, 
+					(float)request.getSession().getAttribute("tp"), 
+					formatter.parse((String)request.getSession().getAttribute("sd")), 
+					(List<OptionalProduct>)request.getSession().getAttribute("ops"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		String path = "/WEB-INF/confirm.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());	
+		templateEngine.process(path, ctx, response.getWriter());
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }

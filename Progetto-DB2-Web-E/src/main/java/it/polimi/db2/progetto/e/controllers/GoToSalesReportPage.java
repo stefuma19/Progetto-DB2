192fa94e84b257710a9d252a84957bc32814d495
aralara <lara.ferro@mail.polimi.it>
2021-12-06 12:23:14 +0100

    Added sales report and logout pages

---
+++ Progetto-DB2-Web-E/src/main/java/it/polimi/db2/progetto/e/controllers/GoToSalesReportPage.java
@@ -0,0 +1,52 @@
+package it.polimi.db2.progetto.e.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@WebServlet("/GoToSalesReportPage")
+public class GoToSalesReportPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private TemplateEngine templateEngine;
+	
+	public GoToSalesReportPage() {
+		super();
+	}
+	
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//TODO: fare
+		
+		String path = "/WEB-INF/salesReport.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		//ctx.setVariable("", );
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
